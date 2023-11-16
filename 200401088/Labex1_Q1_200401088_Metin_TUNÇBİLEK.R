e13d182bd049a4f1cf421aa7d1d1d8f0d1259c72
metin-5115 <tuncbilek.51metin@gmail.com>
2023-11-16 17:56:03 +0300

    1.6, testleri ile tamamlandı

--- 200401088/Labex1_Q1_200401088_Metin_TUNÇBİLEK.R
+++ 200401088/Labex1_Q1_200401088_Metin_TUNÇBİLEK.R
@@ -1,14 +1,23 @@
+#1.1
+
 url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
 download.file(url, destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv", mode = "wb")
+#1.2
 
 maps <- read.csv(url, header = TRUE,sep = ";")
+#1.3
 
 maps$Latitude <- gsub("N", "", (maps$Latitude))
 maps$Latitude<-as.numeric(maps$Latitude)
 
+#1.4
 
 idx <- which(grepl("W", maps$Longitude))
+#1.5
 
 maps$Longitude <- gsub("E","",maps$Longitude)
 maps$Longitude <- gsub("W","",maps$Longitude)
 maps$Longitude <- as.character(maps$Longitude)
+#1.6
+maps$Year <- gsub("AD","",maps$Year)
+maps$Year <- as.numeric(maps$Year)
--- 200401088/Labex1_Q1_tests_200401088_Metin_TUNÇBİLEK.R
+++ 200401088/Labex1_Q1_tests_200401088_Metin_TUNÇBİLEK.R
@@ -3,25 +3,30 @@ library(testthat)
 rm(list = ls())
 
 source("~/GitHub/Vize/200401088/Labex1_Q1_200401088_Metin_TUNÇBİLEK.R")
-
+#1.1.test
 test_that("Test : MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya aktif dizinde mevcuttur.",{
   file_name <- "MapsThatChangedOurWorld_StoryMap_Data.csv"
   expect_true(file.exists(file_name), 
               sprintf("Dosya aktif dizinde mevcut değil.", file_name))
 })
-
+#1.2.test
 test_that("Test : maps adlı değişken Global Workspace’de mevcuttur.", {
   expect_true(exists("maps", envir = .GlobalEnv), info = "maps adlı değişken mevcut değil.")
 })
-
+#1.3.test
 test_that("Test : maps nesnesi bir data.frame'dir.", {
   expect_is(maps, "data.frame", info = "maps nesnesi bir data.frame değil.")
 })
-
+#1.4.test
 test_that("Test : maps adlı data.frame'in ilk sütunu 'City' olmalıdır.", {
   expect_equal(colnames(maps)[1], "City", info = "İlk sütun 'City' değil.")
 })
-
+#1.5.test
 test_that("Test : maps adlı data.frame'in 5. sütununun adında 'Title' kelimesi geçmelidir.", {
   expect_true(grepl("Title", colnames(maps)[5], fixed = TRUE), info = "5. sütunun adında 'Title' kelimesi geçmiyor.")
+})
+
+#1.6.test
+test_that("Test : Latitude adlı sütun numeric değerlerden oluşmalıdır.", {
+  expect_is(maps$Latitude, "numeric", info = "Latitude sütunu numeric değerlerden oluşmuyor.")
 })
